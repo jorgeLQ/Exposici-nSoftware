51920c4fc169e4e34c974a75ee79e009eadaf978
JORGE LUIS QUILUMBA TOAQUIZA <jorge.quilumba@epn.edu.ec>
2019-10-13 17:55:23 -0500

    agregue un progressBar

--- softwareII/softwareII/Form1.Designer.cs
+++ softwareII/softwareII/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // timer1
@@ -47,6 +48,13 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(303, 361);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(193, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -54,6 +62,7 @@
             this.BackgroundImage = global::softwareII.Properties.Resources.INGSW;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "BIENVENIDO";
@@ -65,6 +74,7 @@
         #endregion
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
