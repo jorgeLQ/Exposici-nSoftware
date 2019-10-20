96214b93e090dc181a0fd0d448fd7ce594d775b6
DAVICHO <DAVICHO@DESKTOP-BCUNUEN>
2019-10-19 20:52:57 -0500

    inserte interfaz de Lenin

--- softwareII/softwareII/Form1.cs
+++ softwareII/softwareII/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace softwareII
 {
-    public partial class Form1 : Form
+    public partial class LENIN : Form
     {
-        public Form1()
+        public LENIN()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnS_Click(object sender, EventArgs e)
+        {
+            txtN3.Text = (Convert.ToInt32(txtN1.Text) + Convert.ToInt32(txtN2.Text)).ToString();
         }
     }
 }
