fe8cc2465e0cb5f7c9d33d010ca1f5b80a9b5621
JORGE LUIS QUILUMBA TOAQUIZA <jorge.quilumba@epn.edu.ec>
2019-10-21 09:14:07 -0500

    union de los dos módulos form1 y form2

--- softwareII/softwareII/Form2.Designer.cs
+++ softwareII/softwareII/Form2.Designer.cs
@@ -42,9 +42,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(129, 117);
+            this.label1.Location = new System.Drawing.Point(172, 144);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 29);
+            this.label1.Size = new System.Drawing.Size(295, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "PRIMER NÚMERO:";
             // 
@@ -52,9 +53,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(129, 226);
+            this.label2.Location = new System.Drawing.Point(172, 278);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(268, 29);
+            this.label2.Size = new System.Drawing.Size(329, 36);
             this.label2.TabIndex = 1;
             this.label2.Text = "SEGUNDO NÚMERO:";
             // 
@@ -62,42 +64,47 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(129, 338);
+            this.label3.Location = new System.Drawing.Point(172, 416);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 29);
+            this.label3.Size = new System.Drawing.Size(216, 36);
             this.label3.TabIndex = 2;
             this.label3.Text = "RESULTADO:";
             // 
             // txtN1
             // 
             this.txtN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtN1.Location = new System.Drawing.Point(441, 117);
+            this.txtN1.Location = new System.Drawing.Point(588, 144);
+            this.txtN1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtN1.Name = "txtN1";
-            this.txtN1.Size = new System.Drawing.Size(100, 31);
+            this.txtN1.Size = new System.Drawing.Size(132, 37);
             this.txtN1.TabIndex = 3;
             // 
             // txtN2
             // 
             this.txtN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtN2.Location = new System.Drawing.Point(441, 226);
+            this.txtN2.Location = new System.Drawing.Point(588, 278);
+            this.txtN2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtN2.Name = "txtN2";
-            this.txtN2.Size = new System.Drawing.Size(100, 31);
+            this.txtN2.Size = new System.Drawing.Size(132, 37);
             this.txtN2.TabIndex = 4;
             // 
             // txtN3
             // 
             this.txtN3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtN3.Location = new System.Drawing.Point(441, 338);
+            this.txtN3.Location = new System.Drawing.Point(588, 416);
+            this.txtN3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtN3.Name = "txtN3";
-            this.txtN3.Size = new System.Drawing.Size(100, 31);
+            this.txtN3.Size = new System.Drawing.Size(132, 37);
             this.txtN3.TabIndex = 5;
             // 
             // SUMAR
             // 
             this.SUMAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SUMAR.Location = new System.Drawing.Point(595, 142);
+            this.SUMAR.Location = new System.Drawing.Point(793, 175);
+            this.SUMAR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SUMAR.Name = "SUMAR";
-            this.SUMAR.Size = new System.Drawing.Size(137, 79);
+            this.SUMAR.Size = new System.Drawing.Size(183, 97);
             this.SUMAR.TabIndex = 6;
             this.SUMAR.Text = "SUMAR";
             this.SUMAR.UseVisualStyleBackColor = true;
@@ -105,10 +112,10 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.SUMAR);
             this.Controls.Add(this.txtN3);
             this.Controls.Add(this.txtN2);
@@ -116,8 +123,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
